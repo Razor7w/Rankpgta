c491c30f4d13f758ad9c088440707eb68e535611
Seba Carroza O <seba.carroza@gmail.com>
2019-12-09 23:16:39 -0300

    Feat: - Se cambia el icono del botón

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,10 @@
       <v-app-bar-nav-icon @click.stop="changeDrawerLeft" />
       <v-toolbar-title>{{ $t("app_name") }}</v-toolbar-title>
       <v-spacer />
-      <v-app-bar-nav-icon @click.stop="changeDrawerRight" />
+      <v-btn text icon @click.stop="changeDrawerRight">
+              <v-icon>mdi-account</v-icon>
+            </v-btn>
+      <!-- <v-app-bar-nav-icon  /> -->
     </v-app-bar>
 </template>
 
