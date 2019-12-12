2df83bdf5a9c5559d588b58af02d341c661fc722
Seba Carroza O <seba.carroza@gmail.com>
2019-12-12 01:02:06 -0300

    Feat: - Se deja V Región en el footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,6 +7,7 @@
       <span class="overline mr-2">by</span>
       <span class="font-italic font-weight-bold">Razor7w</span>
       <v-spacer />
+      <span class="mr-2">V Región</span>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
 </template>
