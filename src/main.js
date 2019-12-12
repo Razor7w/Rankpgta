64367b02c584863bce2f84652b2006b8b0302f3e
Seba Carroza O <seba.carroza@gmail.com>
2019-12-12 01:01:44 -0300

    Feat: - Se agrega validacion para la ruta para la capa de idioma

--- src/main.js
+++ src/main.js
@@ -7,6 +7,20 @@ import i18n from './i18n'
 
 Vue.config.productionTip = false
 
+//use beforeEach route guard to set the lenguage
+router.beforeEach((to, from, next) => {
+
+  //use the lenguague from the routing param or default language
+  let language = to.params.lang;
+  if(!language){
+    language = 'es'
+  }
+
+  //set the current language for i18n.
+  i18n.locale = language
+  next()
+})
+
 new Vue({
   router,
   store,
