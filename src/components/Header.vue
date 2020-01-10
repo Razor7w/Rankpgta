b9d4fb609f9fcfbc50fa96dbfcdfca161f41e2f5
Seba Carroza O <seba.carroza@gmail.com>
2020-01-09 23:25:58 -0300

    Feat: - Se modifica el header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,25 +6,47 @@
       dark
     >
       <v-app-bar-nav-icon @click.stop="changeDrawerLeft" />
-      <v-toolbar-title>{{ $t("app_name") }}</v-toolbar-title>
+      <v-toolbar-title @click="GoHome">{{ $t("app_name") }}</v-toolbar-title>
       <v-spacer />
-      <v-btn text icon @click.stop="changeDrawerRight">
-              <v-icon>mdi-account</v-icon>
-            </v-btn>
+      <v-btn text @click.stop="changeDrawerRight" v-if="isAuthenticated">
+          <v-avatar size="36" class="mr-2">
+            <img
+              :src="usuario.photoUrl"
+              alt="John"
+            >
+          </v-avatar>
+          <span>{{ usuario.nombre }}</span>
+          <v-icon>mdi-chevron-down</v-icon>
+      </v-btn>
+      <v-btn text v-else @click="GoIngreso">
+          <v-icon>mdi-account</v-icon>
+          <span>Iniciar Sesión</span>
+      </v-btn>
       <!-- <v-app-bar-nav-icon  /> -->
     </v-app-bar>
 </template>
 
 <script>
-    import { mapMutations } from 'vuex'
+    import { mapMutations, mapState } from 'vuex'
     export default {
       data () {
-      return {
-      
-      }
-    },
-    methods:{
-      ...mapMutations(['changeDrawerLeft', 'changeDrawerRight'])
-    }
+        return {
+        
+        }
+      },
+      methods:{
+        ...mapMutations(['changeDrawerLeft', 'changeDrawerRight']),
+        GoHome(){
+          this.$router.push(`/${this.$i18n.locale}/`).catch(err => {})
+        },
+        GoIngreso(){
+          this.$router.push(`/${this.$i18n.locale}/ingreso`).catch(err => {})
+        }
+      },
+      computed: {
+        ...mapState(['isAuthenticated', 'dataUser', 'usuario'])
+      },
+      mounted() {
+      },
     }
 </script>
