caddbca853b431021143fd9f4154264aacd1fe95
Seba Carroza O <seba.carroza@gmail.com>
2020-01-09 23:18:59 -0300

    Feat: - Se cambia de getters a usar los state directos - Se crean funciones para trabajar con el usuario

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { auth} from "@/firebase"
+import router from '@/router'
+import i18n from '@/i18n'
 
 Vue.use(Vuex)
 
@@ -9,20 +12,10 @@ export default new Vuex.Store({
     drawerRight: false,
     subDrawerRight: false,
     subDrawerLeft: false,
+    isAuthenticated: false,
+    usuario: {}
   },
   getters:{
-    drawerLeft(state){
-      return state.drawerLeft
-    },
-    drawerRight(state){
-      return state.drawerRight
-    },
-    subDrawerRight(state){
-      return state.subDrawerRight
-    },
-    subDrawerLeft(state){
-      return state.subDrawerLeft
-    },
   },
   mutations: {
     changeDrawerLeft(state, payload){
@@ -44,9 +37,26 @@ export default new Vuex.Store({
     },
     changeSubDrawerLeft(state, payload){
       state.subDrawerLeft = payload
+    },
+    nuevoUsuario(state, payload){
+      state.isAuthenticated = (payload === null) ? false : true
+      state.usuario = payload
     }
   },
   actions: {
+    setUsuario({commit}, user){
+      const usuario = {
+        nombre   : user.displayName,
+        email    : user.email,
+        uid      : user.uid,
+        photoUrl : user.photoURL
+      }
+      commit('nuevoUsuario', usuario)
+    },
+    cerrarSesion({commit}){
+      auth.signOut()
+      commit('nuevoUsuario', null)
+    }
   },
   modules: {
   }
